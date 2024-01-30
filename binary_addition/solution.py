90b917164aeb1e21f6bd2a260f49cdd09ed08090
Dhruvgit93 <138569419+Dhruvgit93@users.noreply.github.com>
2024-01-30 13:44:35 +0530

    5th question join string and remove repeated letterand sort in alphabatical order

--- binary_addition/solution.py
+++ binary_addition/solution.py
@@ -1,7 +1,5 @@
 def add_binary(a,b):
     return bin(a+b)[2:]
-s='-1 2 3 4 -5'
-print(min([int(x) for x in s.split()]))
 
 print(add_binary(1,1),
       add_binary(0,1),
---
+++ two_to_one_sorted_string/solution.py
@@ -0,0 +1,5 @@
+def longest(a1, a2):
+    return "".join(sorted(set(a1+a2)))
+
+print(longest("aretheyhere", "yestheyarehere")=="aehrsty",
+      longest("loopingisfunbutdangerous", "lessdangerousthancoding")=="abcdefghilnoprstu")
